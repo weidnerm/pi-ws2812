9d5f91282823e0d4b42256872c9d1ff6e8af2bf8
Michael Weidner <michael.weidner@motorolasolutions.com>
2017-04-02 18:45:21 -0400

    preliminary support for spotlight movement

--- Sequences/globe_gen.py
+++ Sequences/globe_gen.py
@@ -2,32 +2,20 @@ import math
 
 leds = [68, 47, 79]
 
-def main():
-
-	print("do")
-	for index in xrange(255):
-		print("    rainbow 1,1,47,1,%d,%d" % ((index+0*255/3)%255,(index+0*255/3)%255) )
-		print("    rainbow 1,1,68,1,%d,%d" % ((index+0*255/3)%255,(index+0*255/3)%255) )
-		print("    rainbow 1,1,79,1,%d,%d" % ((index+0*255/3)%255,(index+0*255/3)%255) )
-		#~ if index == 25:
-			#~ print("    fill 1,ffffff,47,1")
-		#~ if index == 30:
-			#~ print("    fill 1,ffffff,68,1")
-		#~ if index == 60:
-			#~ print("    fill 1,ffffff,79,1")
-		print("    render")
-		print("    delay 25")
-	print("loop")
-
-
 def fadeColor(colorRGB, duration, fadeIn):
 	base = 1.0
 	logBright = []
 	brightnessTransSteps = int(duration/0.025)  # 25 msec steps
 	scaleFactor = math.pow(10,math.log10(255)/brightnessTransSteps)
 	base = scaleFactor
-	#~ print("# brightnessTransSteps=%d" % brightnessTransSteps)
-	#~ print("# scaleFactor=%f" % scaleFactor)
+
+	if fadeIn:
+		fadeText = "In"
+	else:
+		fadeText = "Out"
+	print( "#" )
+	print( "# Fade color %06x %s over %f seconds" % (colorRGB, fadeText, duration ) )
+
 	for index in xrange(brightnessTransSteps):
 		logBright.append(int(base))
 		base = base * scaleFactor
@@ -49,91 +37,77 @@ def fadeColor(colorRGB, duration, fadeIn):
 		
 
 
-def main2():
-	base = 1.0
-	logBright = []
-	brightnessTransSteps = 40
-	scaleFactor = math.log(255)/brightnessTransSteps
-	for index in xrange(brightnessTransSteps):
-		logBright.append(int(base))
-		base = base * scaleFactor
+def timeRainbow(duration, repeats=1):
+	steps = int(duration/0.025)  # 25 msec steps
 
-	print("do")
-	for reps in xrange(3):
-		for index in xrange(255):
-			ledBrightnesses = []
-			for ledindex in xrange(len(leds)):
-				ledBrightnesses.append(0)
-				
-			#~ if index >= 0 and index < 64:
-				#~ ledBrightnesses[0] = 255
-			#~ if index >= 64 and index < 128:
-				#~ ledBrightnesses[1] = 255
-			#~ if index >= 128 and index < 192:
-				#~ ledBrightnesses[2] = 255
+	print( "#" )
+	print( "# Rainbow for %f seconds repeated %d times" % (duration,repeats) )
+	print( "brightness 1,%d;" % (255) )
+	print( "do" )
+	for index in xrange(steps):
+		outText = "    "
+		wheelFrac = float(index)/float(steps)*256
+		for ledIndex in xrange(len(leds)):
+			outText = outText + "rainbow 1,1,%d,1,%d,%d;" % ( leds[ledIndex], wheelFrac,wheelFrac) 
+		outText = outText + "render;"
+		outText = outText + "delay 25"
+		print(outText)
+	print( "loop %d" % (repeats) )
 
 
-			if index >= 0 and index < 64:
-				ledBrightnesses[0] = logBright[index-0]
-			if index >= 64 and index < 128:
-				ledBrightnesses[0] = logBright[127-index]
-				ledBrightnesses[1] = logBright[index-64]
-			if index >= 128 and index < 192:
-				ledBrightnesses[1] = logBright[191-index]
-				ledBrightnesses[2] = logBright[index-128]
-			if index >= 192 and index < 256:
-				ledBrightnesses[2] = logBright[255-index]
+	
+def spotlightMove(duration, repeats=1):
+	rampDuration = duration/len(leds)
+	print("brightness 1,255")
+	print("fill 1")
+
+	print( "#" )
+	print( "# Ramp up spotlight over %f seconds" % (rampDuration) )
+	__rampLedPair(rampDuration, 0, True, False)
+	
+	print( "#" )
+	print( "# sweep spotlight over %f seconds %d times" % (duration,repeats) )
+	print( "do" )
+	for index in xrange(len(leds)):
+		__rampLedPair(rampDuration, index+1, True, True)
+	print( "loop %d" % repeats )
+	
+	print( "#" )
+	print( "# Ramp down spotlight over %f seconds" % (rampDuration) )
+	__rampLedPair(rampDuration, 1, False, True)
+
+def __rampLedPair(duration, brightLedIndex, doBright, doDim):
+	steps = int(duration/0.025)  # 25 msec steps
+
+	for index in xrange(steps):
+		outText = "    "
+		timeFrac = float(index)/float(steps)
+		
+		if doBright:
+			outText = outText + __getCmdForLEDBrightness(timeFrac, brightLedIndex)
+		if doDim:
+			outText = outText + __getCmdForLEDBrightness(1.0-timeFrac, (brightLedIndex-1) % len(leds) )
 			
-			for ledindex in xrange(len(leds)):
-				if ledBrightnesses[ledindex] >= 256:
-					ledBrightnesses[ledindex] = 255 # cap the brightness
-						
-			for ledindex in xrange(len(leds)):
-				print("    fill 1,%02x%02x%02x,%d,1" % ( 
-					ledBrightnesses[ledindex]*(reps==0),
-					ledBrightnesses[ledindex]*(reps==1),
-					ledBrightnesses[ledindex]*(reps==2),
-					leds[ledindex]
-					))
-
-			print("    render")
-			print("    delay 25")
+		outText = outText + "render;"
+		outText = outText + "delay 25"
+		print(outText)
+		
 
 
-		#~ for index in xrange(255,0,-1):
-			#~ ledBrightnesses = []
-			#~ for ledindex in xrange(len(leds)):
-				#~ ledBrightnesses.append(0)
-#~ 
-#~ 
-			#~ if index >= 0 and index < 64:
-				#~ ledBrightnesses[0] = (index-0)*4
-			#~ if index >= 64 and index < 128:
-				#~ ledBrightnesses[0] = (128-index)*4
-				#~ ledBrightnesses[1] = (index-64)*4
-			#~ if index >= 128 and index < 192:
-				#~ ledBrightnesses[1] = (192-index)*4
-				#~ ledBrightnesses[2] = (index-128)*4
-			#~ if index >= 192 and index < 256:
-				#~ ledBrightnesses[2] = (256-index)*4
-			#~ 
-			#~ for ledindex in xrange(len(leds)):
-				#~ if ledBrightnesses[ledindex] >= 256:
-					#~ ledBrightnesses[ledindex] = 255 # cap the brightness
-						#~ 
-			#~ for ledindex in xrange(len(leds)):
-				#~ print("    fill 1,%02x%02x%02x,%d,1" % ( 
-					#~ ledBrightnesses[ledindex],
-					#~ ledBrightnesses[ledindex],
-					#~ ledBrightnesses[ledindex],
-					#~ leds[ledindex]
-					#~ ))
-#~ 
-			#~ print("    render")
-			#~ print("    delay 25")
+def __getCmdForLEDBrightness(brightFrac, ledIndex):
+	outText = ""
+	
+	rawBright = int(brightFrac*256)
+	
+	if rawBright >= 256:
+		rawBright = 255 # cap the brightness
+	
+	outText = outText + "fill 1,%02x%02x%02x,%d,1;" % (
+		rawBright,rawBright,rawBright,
+		leds[ledIndex % len(leds)] )
+	return outText
+	
 
-
-	print("loop")
 
 def setup():
 	print("setup channel_1_count=150")
@@ -141,19 +115,34 @@ def setup():
 	print("fill 1")
 	print("render")
 
+def do():
+	print("do")
+	
+def loop(count=0):
+	if count == 0:
+		print("loop")
+	else:
+		print("loop %d" % count)
 
 if __name__ == "__main__":
-    setup();
-    fadeColor(0xff0000, 1.0, True)
-    fadeColor(0xff0000, 1.0, False)
-    fadeColor(0xed3000, 1.0, True)
-    fadeColor(0xed3000, 1.0, False)
-    fadeColor(0xffc000, 1.0, True)
-    fadeColor(0xffc000, 1.0, False)
-    fadeColor(0x00ff00, 1.0, True)
-    fadeColor(0x00ff00, 1.0, False)
-    fadeColor(0x0000ff, 1.0, True)
-    fadeColor(0x0000ff, 1.0, False)
-    fadeColor(0xff00ff, 1.0, True)
-    fadeColor(0xff00ff, 1.0, False)
-    #~ main2();
+	setup();
+	do()
+	spotlightMove(3.0, 4)
+
+	fadeColor(0xff0000, 1.0, True)
+	timeRainbow(5.0, 3)
+	fadeColor(0xff0000, 1.0, False)
+
+	fadeColor(0xff0000, 1.0, True)
+	fadeColor(0xff0000, 1.0, False)
+	fadeColor(0xed3000, 1.0, True)
+	fadeColor(0xed3000, 1.0, False)
+	fadeColor(0xffc000, 1.0, True)
+	fadeColor(0xffc000, 1.0, False)
+	fadeColor(0x00ff00, 1.0, True)
+	fadeColor(0x00ff00, 1.0, False)
+	fadeColor(0x0000ff, 1.0, True)
+	fadeColor(0x0000ff, 1.0, False)
+	fadeColor(0xff00ff, 1.0, True)
+	fadeColor(0xff00ff, 1.0, False)
+	loop()

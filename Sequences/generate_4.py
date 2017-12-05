4b4de3c48ea3d1ecc521be991e8374dde31bd207
Michael Weidner <michael.weidner@motorolasolutions.com>
2017-12-05 11:49:44 -0500

    add raj random color generator tool.

--- Sequences/generate_4.py
+++ Sequences/generate_4.py
@@ -103,6 +103,55 @@ class Generate_Sequence:
         self.fileHandle.write("loop\n")
         self.close_sequence_file()
     
+    def generate_color_full_rainbow_2(self, which_seq):
+        delayDuration = 25
+        colorDegs = -1
+
+        self.create_file_with_header(which_seq+".txt")
+        self.fileHandle.write("do\n")
+        
+        for index in range(120):
+            last_color_degs = colorDegs
+            while last_color_degs == colorDegs: # make it random and different. seems like it stutters when same multiple times in a row.
+                colorDegs = (random.randint(0,12)*255/12 ) %255
+
+            self.fileHandle.write("do\n")
+
+            self.fileHandle.write('    rotate 1,6,%d,0,LEN,%s\n' % ( index%2, self.deg2color(colorDegs) ) )
+            self.fileHandle.write("render\n")
+            self.fileHandle.write("delay %d\n" % delayDuration)
+            self.fileHandle.write("loop 60\n")
+
+        self.fileHandle.write("loop\n")
+        self.close_sequence_file()
+    
+    def generate_3d_test(self, which_seq):
+        delayDuration = 25
+
+        self.create_file_with_header(which_seq+".txt")
+        self.fileHandle.write("do\n")
+        
+        for pos in range(3):
+            center = 80
+            delayDuration = 250
+            output = '    fill 1\n'
+            self.fileHandle.write(output)
+            if pos == 0:
+                output = '    fill 1,%s,%d,1\n' % ( 'ff00ff', center-pos )
+                self.fileHandle.write(output)
+            else:
+                output = '    fill 1,%s,%d,1\n' % ( '0000ff', center-pos )
+                self.fileHandle.write(output)
+                output = '    fill 1,%s,%d,1\n' % ( 'ff0000', center+pos )
+                self.fileHandle.write(output)
+            
+            
+            self.fileHandle.write("render\n")
+            self.fileHandle.write("delay %d\n" % delayDuration)
+
+        self.fileHandle.write("loop\n")
+        self.close_sequence_file()
+    
     def handle_generate(self, which_seq):
         if which_seq == 'color_bonanza':
             self.generate_color_bonanza_1(which_seq)
@@ -110,12 +159,16 @@ class Generate_Sequence:
             self.generate_color_bonanza_2(which_seq)
         elif which_seq == 'color_full_rainbow':
             self.generate_color_full_rainbow_1(which_seq)
+        elif which_seq == 'color_full_rainbow_2':
+            self.generate_color_full_rainbow_2(which_seq)
+        elif which_seq == '3d_test':
+            self.generate_3d_test(which_seq)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate sequences.')
     parser.add_argument('-n', '--num_leds', default=240, help='number of leds in the list.')
-    parser.add_argument('-w', '--which_seq', default='color_bonanza', choices=['color_bonanza','color_full_rainbow','color_bonanza_2'], help='which sequence to generate')
+    parser.add_argument('-w', '--which_seq', default='color_bonanza', choices=['color_bonanza','color_full_rainbow','color_full_rainbow_2','color_bonanza_2','3d_test'], help='which sequence to generate')
 
     args = parser.parse_args()
     myGenerate_Sequence = Generate_Sequence(args.num_leds)
--- main.c
+++ main.c
@@ -66,7 +66,7 @@
 //#define STRIP_TYPE                             WS2811_STRIP_GBR       // WS2812/SK6812RGB integrated chip+leds
 //#define STRIP_TYPE                             SK6812_STRIP_RGBW      // SK6812RGBW (NOT SK6812RGB)
 
-#define WIDTH                                    150
+#define WIDTH                                    450
 #define HEIGHT                                   1
 #define LED_COUNT                                (WIDTH * HEIGHT)
 

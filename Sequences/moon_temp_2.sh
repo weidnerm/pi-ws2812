8c2de93652d047d522d3c0fea1313b19f91573d4
Michael Weidner <michael.weidner@motorolasolutions.com>
2019-11-10 10:14:11 -0500

    more moon stuff

---
+++ Sequences/moon_temp_2.sh
@@ -0,0 +1,1148 @@
+setup channel_1_count=53
+brightness 1,128
+
+
+fill 1
+fill 1,000000,0,52
+render
+delay 200
+
+fill 1
+fill 1,000000,0,33
+fill 1,010101,33,1
+fill 1,000000,34,18
+render
+delay 200
+
+fill 1
+fill 1,000000,0,32
+fill 1,010101,32,1
+fill 1,030303,33,1
+fill 1,010101,34,1
+fill 1,000000,35,17
+render
+delay 200
+
+fill 1
+fill 1,000000,0,31
+fill 1,010101,31,1
+fill 1,030303,32,1
+fill 1,070707,33,1
+fill 1,030303,34,1
+fill 1,010101,35,1
+fill 1,000000,36,16
+render
+delay 200
+
+fill 1
+fill 1,000000,0,30
+fill 1,010101,30,1
+fill 1,030303,31,1
+fill 1,070707,32,1
+fill 1,0f0f0f,33,1
+fill 1,070707,34,1
+fill 1,030303,35,1
+fill 1,010101,36,1
+fill 1,000000,37,15
+render
+delay 200
+
+fill 1
+fill 1,000000,0,29
+fill 1,010101,29,1
+fill 1,030303,30,1
+fill 1,070707,31,1
+fill 1,0f0f0f,32,1
+fill 1,1f1f1f,33,1
+fill 1,0f0f0f,34,1
+fill 1,070707,35,1
+fill 1,030303,36,1
+fill 1,010101,37,1
+fill 1,000000,38,14
+render
+delay 200
+
+fill 1
+fill 1,000000,0,28
+fill 1,010101,28,1
+fill 1,030303,29,1
+fill 1,070707,30,1
+fill 1,0f0f0f,31,1
+fill 1,1f1f1f,32,1
+fill 1,3f3f3f,33,1
+fill 1,1f1f1f,34,1
+fill 1,0f0f0f,35,1
+fill 1,070707,36,1
+fill 1,030303,37,1
+fill 1,010101,38,1
+fill 1,000000,39,13
+render
+delay 200
+
+fill 1
+fill 1,000000,0,27
+fill 1,010101,27,1
+fill 1,030303,28,1
+fill 1,070707,29,1
+fill 1,0f0f0f,30,1
+fill 1,1f1f1f,31,1
+fill 1,3f3f3f,32,1
+fill 1,7f7f7f,33,1
+fill 1,3f3f3f,34,1
+fill 1,1f1f1f,35,1
+fill 1,0f0f0f,36,1
+fill 1,070707,37,1
+fill 1,030303,38,1
+fill 1,010101,39,1
+fill 1,000000,40,12
+render
+delay 200
+
+fill 1
+fill 1,000000,0,26
+fill 1,010101,26,1
+fill 1,030303,27,1
+fill 1,070707,28,1
+fill 1,0f0f0f,29,1
+fill 1,1f1f1f,30,1
+fill 1,3f3f3f,31,1
+fill 1,7f7f7f,32,1
+fill 1,ffffff,33,1
+fill 1,7f7f7f,34,1
+fill 1,3f3f3f,35,1
+fill 1,1f1f1f,36,1
+fill 1,0f0f0f,37,1
+fill 1,070707,38,1
+fill 1,030303,39,1
+fill 1,010101,40,1
+fill 1,000000,41,11
+render
+delay 200
+
+fill 1
+fill 1,000000,0,25
+fill 1,010101,25,1
+fill 1,030303,26,1
+fill 1,070707,27,1
+fill 1,0f0f0f,28,1
+fill 1,1f1f1f,29,1
+fill 1,3f3f3f,30,1
+fill 1,7f7f7f,31,1
+fill 1,ffffff,32,3
+fill 1,7f7f7f,35,1
+fill 1,3f3f3f,36,1
+fill 1,1f1f1f,37,1
+fill 1,0f0f0f,38,1
+fill 1,070707,39,1
+fill 1,030303,40,1
+fill 1,010101,41,1
+fill 1,000000,42,10
+render
+delay 200
+
+fill 1
+fill 1,000000,0,24
+fill 1,010101,24,1
+fill 1,030303,25,1
+fill 1,070707,26,1
+fill 1,0f0f0f,27,1
+fill 1,1f1f1f,28,1
+fill 1,3f3f3f,29,1
+fill 1,7f7f7f,30,1
+fill 1,ffffff,31,5
+fill 1,7f7f7f,36,1
+fill 1,3f3f3f,37,1
+fill 1,1f1f1f,38,1
+fill 1,0f0f0f,39,1
+fill 1,070707,40,1
+fill 1,030303,41,1
+fill 1,010101,42,1
+fill 1,000000,43,9
+render
+delay 200
+
+fill 1
+fill 1,000000,0,23
+fill 1,010101,23,1
+fill 1,030303,24,1
+fill 1,070707,25,1
+fill 1,0f0f0f,26,1
+fill 1,1f1f1f,27,1
+fill 1,3f3f3f,28,1
+fill 1,7f7f7f,29,1
+fill 1,ffffff,30,7
+fill 1,7f7f7f,37,1
+fill 1,3f3f3f,38,1
+fill 1,1f1f1f,39,1
+fill 1,0f0f0f,40,1
+fill 1,070707,41,1
+fill 1,030303,42,1
+fill 1,010101,43,1
+fill 1,000000,44,8
+render
+delay 200
+
+fill 1
+fill 1,000000,0,22
+fill 1,010101,22,1
+fill 1,030303,23,1
+fill 1,070707,24,1
+fill 1,0f0f0f,25,1
+fill 1,1f1f1f,26,1
+fill 1,3f3f3f,27,1
+fill 1,7f7f7f,28,1
+fill 1,ffffff,29,9
+fill 1,7f7f7f,38,1
+fill 1,3f3f3f,39,1
+fill 1,1f1f1f,40,1
+fill 1,0f0f0f,41,1
+fill 1,070707,42,1
+fill 1,030303,43,1
+fill 1,010101,44,1
+fill 1,000000,45,7
+render
+delay 200
+
+fill 1
+fill 1,000000,0,21
+fill 1,010101,21,1
+fill 1,030303,22,1
+fill 1,070707,23,1
+fill 1,0f0f0f,24,1
+fill 1,1f1f1f,25,1
+fill 1,3f3f3f,26,1
+fill 1,7f7f7f,27,1
+fill 1,ffffff,28,11
+fill 1,7f7f7f,39,1
+fill 1,3f3f3f,40,1
+fill 1,1f1f1f,41,1
+fill 1,0f0f0f,42,1
+fill 1,070707,43,1
+fill 1,030303,44,1
+fill 1,010101,45,1
+fill 1,000000,46,6
+render
+delay 200
+
+fill 1
+fill 1,000000,0,20
+fill 1,010101,20,1
+fill 1,030303,21,1
+fill 1,070707,22,1
+fill 1,0f0f0f,23,1
+fill 1,1f1f1f,24,1
+fill 1,3f3f3f,25,1
+fill 1,7f7f7f,26,1
+fill 1,ffffff,27,13
+fill 1,7f7f7f,40,1
+fill 1,3f3f3f,41,1
+fill 1,1f1f1f,42,1
+fill 1,0f0f0f,43,1
+fill 1,070707,44,1
+fill 1,030303,45,1
+fill 1,010101,46,1
+fill 1,000000,47,5
+render
+delay 200
+
+fill 1
+fill 1,000000,0,19
+fill 1,010101,19,1
+fill 1,030303,20,1
+fill 1,070707,21,1
+fill 1,0f0f0f,22,1
+fill 1,1f1f1f,23,1
+fill 1,3f3f3f,24,1
+fill 1,7f7f7f,25,1
+fill 1,ffffff,26,15
+fill 1,7f7f7f,41,1
+fill 1,3f3f3f,42,1
+fill 1,1f1f1f,43,1
+fill 1,0f0f0f,44,1
+fill 1,070707,45,1
+fill 1,030303,46,1
+fill 1,010101,47,1
+fill 1,000000,48,4
+render
+delay 200
+
+fill 1
+fill 1,000000,0,18
+fill 1,010101,18,1
+fill 1,030303,19,1
+fill 1,070707,20,1
+fill 1,0f0f0f,21,1
+fill 1,1f1f1f,22,1
+fill 1,3f3f3f,23,1
+fill 1,7f7f7f,24,1
+fill 1,ffffff,25,17
+fill 1,7f7f7f,42,1
+fill 1,3f3f3f,43,1
+fill 1,1f1f1f,44,1
+fill 1,0f0f0f,45,1
+fill 1,070707,46,1
+fill 1,030303,47,1
+fill 1,010101,48,1
+fill 1,000000,49,3
+render
+delay 200
+
+fill 1
+fill 1,000000,0,17
+fill 1,010101,17,1
+fill 1,030303,18,1
+fill 1,070707,19,1
+fill 1,0f0f0f,20,1
+fill 1,1f1f1f,21,1
+fill 1,3f3f3f,22,1
+fill 1,7f7f7f,23,1
+fill 1,ffffff,24,19
+fill 1,7f7f7f,43,1
+fill 1,3f3f3f,44,1
+fill 1,1f1f1f,45,1
+fill 1,0f0f0f,46,1
+fill 1,070707,47,1
+fill 1,030303,48,1
+fill 1,010101,49,1
+fill 1,000000,50,2
+render
+delay 200
+
+fill 1
+fill 1,000000,0,16
+fill 1,010101,16,1
+fill 1,030303,17,1
+fill 1,070707,18,1
+fill 1,0f0f0f,19,1
+fill 1,1f1f1f,20,1
+fill 1,3f3f3f,21,1
+fill 1,7f7f7f,22,1
+fill 1,ffffff,23,21
+fill 1,7f7f7f,44,1
+fill 1,3f3f3f,45,1
+fill 1,1f1f1f,46,1
+fill 1,0f0f0f,47,1
+fill 1,070707,48,1
+fill 1,030303,49,1
+fill 1,010101,50,1
+fill 1,000000,51,1
+render
+delay 200
+
+fill 1
+fill 1,000000,0,15
+fill 1,010101,15,1
+fill 1,030303,16,1
+fill 1,070707,17,1
+fill 1,0f0f0f,18,1
+fill 1,1f1f1f,19,1
+fill 1,3f3f3f,20,1
+fill 1,7f7f7f,21,1
+fill 1,ffffff,22,23
+fill 1,7f7f7f,45,1
+fill 1,3f3f3f,46,1
+fill 1,1f1f1f,47,1
+fill 1,0f0f0f,48,1
+fill 1,070707,49,1
+fill 1,030303,50,1
+fill 1,010101,51,1
+render
+delay 200
+
+fill 1
+fill 1,000000,0,14
+fill 1,010101,14,1
+fill 1,030303,15,1
+fill 1,070707,16,1
+fill 1,0f0f0f,17,1
+fill 1,1f1f1f,18,1
+fill 1,3f3f3f,19,1
+fill 1,7f7f7f,20,1
+fill 1,ffffff,21,25
+fill 1,7f7f7f,46,1
+fill 1,3f3f3f,47,1
+fill 1,1f1f1f,48,1
+fill 1,0f0f0f,49,1
+fill 1,070707,50,1
+fill 1,030303,51,1
+render
+delay 200
+
+fill 1
+fill 1,010101,0,1
+fill 1,000000,1,12
+fill 1,010101,13,1
+fill 1,030303,14,1
+fill 1,070707,15,1
+fill 1,0f0f0f,16,1
+fill 1,1f1f1f,17,1
+fill 1,3f3f3f,18,1
+fill 1,7f7f7f,19,1
+fill 1,ffffff,20,27
+fill 1,7f7f7f,47,1
+fill 1,3f3f3f,48,1
+fill 1,1f1f1f,49,1
+fill 1,0f0f0f,50,1
+fill 1,070707,51,1
+render
+delay 200
+
+fill 1
+fill 1,030303,0,1
+fill 1,010101,1,1
+fill 1,000000,2,10
+fill 1,010101,12,1
+fill 1,030303,13,1
+fill 1,070707,14,1
+fill 1,0f0f0f,15,1
+fill 1,1f1f1f,16,1
+fill 1,3f3f3f,17,1
+fill 1,7f7f7f,18,1
+fill 1,ffffff,19,29
+fill 1,7f7f7f,48,1
+fill 1,3f3f3f,49,1
+fill 1,1f1f1f,50,1
+fill 1,0f0f0f,51,1
+render
+delay 200
+
+fill 1
+fill 1,070707,0,1
+fill 1,030303,1,1
+fill 1,010101,2,1
+fill 1,000000,3,8
+fill 1,010101,11,1
+fill 1,030303,12,1
+fill 1,070707,13,1
+fill 1,0f0f0f,14,1
+fill 1,1f1f1f,15,1
+fill 1,3f3f3f,16,1
+fill 1,7f7f7f,17,1
+fill 1,ffffff,18,31
+fill 1,7f7f7f,49,1
+fill 1,3f3f3f,50,1
+fill 1,1f1f1f,51,1
+render
+delay 200
+
+fill 1
+fill 1,0f0f0f,0,1
+fill 1,070707,1,1
+fill 1,030303,2,1
+fill 1,010101,3,1
+fill 1,000000,4,6
+fill 1,010101,10,1
+fill 1,030303,11,1
+fill 1,070707,12,1
+fill 1,0f0f0f,13,1
+fill 1,1f1f1f,14,1
+fill 1,3f3f3f,15,1
+fill 1,7f7f7f,16,1
+fill 1,ffffff,17,33
+fill 1,7f7f7f,50,1
+fill 1,3f3f3f,51,1
+render
+delay 200
+
+fill 1
+fill 1,1f1f1f,0,1
+fill 1,0f0f0f,1,1
+fill 1,070707,2,1
+fill 1,030303,3,1
+fill 1,010101,4,1
+fill 1,000000,5,4
+fill 1,010101,9,1
+fill 1,030303,10,1
+fill 1,070707,11,1
+fill 1,0f0f0f,12,1
+fill 1,1f1f1f,13,1
+fill 1,3f3f3f,14,1
+fill 1,7f7f7f,15,1
+fill 1,ffffff,16,35
+fill 1,7f7f7f,51,1
+render
+delay 200
+
+fill 1
+fill 1,3f3f3f,0,1
+fill 1,1f1f1f,1,1
+fill 1,0f0f0f,2,1
+fill 1,070707,3,1
+fill 1,030303,4,1
+fill 1,010101,5,1
+fill 1,000000,6,2
+fill 1,010101,8,1
+fill 1,030303,9,1
+fill 1,070707,10,1
+fill 1,0f0f0f,11,1
+fill 1,1f1f1f,12,1
+fill 1,3f3f3f,13,1
+fill 1,7f7f7f,14,1
+fill 1,ffffff,15,37
+render
+delay 200
+
+fill 1
+fill 1,7f7f7f,0,1
+fill 1,3f3f3f,1,1
+fill 1,1f1f1f,2,1
+fill 1,0f0f0f,3,1
+fill 1,070707,4,1
+fill 1,030303,5,1
+fill 1,010101,6,2
+fill 1,030303,8,1
+fill 1,070707,9,1
+fill 1,0f0f0f,10,1
+fill 1,1f1f1f,11,1
+fill 1,3f3f3f,12,1
+fill 1,7f7f7f,13,1
+fill 1,ffffff,14,38
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,1
+fill 1,7f7f7f,1,1
+fill 1,3f3f3f,2,1
+fill 1,1f1f1f,3,1
+fill 1,0f0f0f,4,1
+fill 1,070707,5,1
+fill 1,030303,6,2
+fill 1,070707,8,1
+fill 1,0f0f0f,9,1
+fill 1,1f1f1f,10,1
+fill 1,3f3f3f,11,1
+fill 1,7f7f7f,12,1
+fill 1,ffffff,13,39
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,2
+fill 1,7f7f7f,2,1
+fill 1,3f3f3f,3,1
+fill 1,1f1f1f,4,1
+fill 1,0f0f0f,5,1
+fill 1,070707,6,2
+fill 1,0f0f0f,8,1
+fill 1,1f1f1f,9,1
+fill 1,3f3f3f,10,1
+fill 1,7f7f7f,11,1
+fill 1,ffffff,12,40
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,3
+fill 1,7f7f7f,3,1
+fill 1,3f3f3f,4,1
+fill 1,1f1f1f,5,1
+fill 1,0f0f0f,6,2
+fill 1,1f1f1f,8,1
+fill 1,3f3f3f,9,1
+fill 1,7f7f7f,10,1
+fill 1,ffffff,11,41
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,4
+fill 1,7f7f7f,4,1
+fill 1,3f3f3f,5,1
+fill 1,1f1f1f,6,2
+fill 1,3f3f3f,8,1
+fill 1,7f7f7f,9,1
+fill 1,ffffff,10,42
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,5
+fill 1,7f7f7f,5,1
+fill 1,3f3f3f,6,2
+fill 1,7f7f7f,8,1
+fill 1,ffffff,9,43
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,6
+fill 1,7f7f7f,6,2
+fill 1,ffffff,8,44
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,52
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,33
+fill 1,7f7f7f,33,1
+fill 1,ffffff,34,18
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,32
+fill 1,7f7f7f,32,1
+fill 1,3f3f3f,33,1
+fill 1,7f7f7f,34,1
+fill 1,ffffff,35,17
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,31
+fill 1,7f7f7f,31,1
+fill 1,3f3f3f,32,1
+fill 1,1f1f1f,33,1
+fill 1,3f3f3f,34,1
+fill 1,7f7f7f,35,1
+fill 1,ffffff,36,16
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,30
+fill 1,7f7f7f,30,1
+fill 1,3f3f3f,31,1
+fill 1,1f1f1f,32,1
+fill 1,0f0f0f,33,1
+fill 1,1f1f1f,34,1
+fill 1,3f3f3f,35,1
+fill 1,7f7f7f,36,1
+fill 1,ffffff,37,15
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,29
+fill 1,7f7f7f,29,1
+fill 1,3f3f3f,30,1
+fill 1,1f1f1f,31,1
+fill 1,0f0f0f,32,1
+fill 1,070707,33,1
+fill 1,0f0f0f,34,1
+fill 1,1f1f1f,35,1
+fill 1,3f3f3f,36,1
+fill 1,7f7f7f,37,1
+fill 1,ffffff,38,14
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,28
+fill 1,7f7f7f,28,1
+fill 1,3f3f3f,29,1
+fill 1,1f1f1f,30,1
+fill 1,0f0f0f,31,1
+fill 1,070707,32,1
+fill 1,030303,33,1
+fill 1,070707,34,1
+fill 1,0f0f0f,35,1
+fill 1,1f1f1f,36,1
+fill 1,3f3f3f,37,1
+fill 1,7f7f7f,38,1
+fill 1,ffffff,39,13
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,27
+fill 1,7f7f7f,27,1
+fill 1,3f3f3f,28,1
+fill 1,1f1f1f,29,1
+fill 1,0f0f0f,30,1
+fill 1,070707,31,1
+fill 1,030303,32,1
+fill 1,010101,33,1
+fill 1,030303,34,1
+fill 1,070707,35,1
+fill 1,0f0f0f,36,1
+fill 1,1f1f1f,37,1
+fill 1,3f3f3f,38,1
+fill 1,7f7f7f,39,1
+fill 1,ffffff,40,12
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,26
+fill 1,7f7f7f,26,1
+fill 1,3f3f3f,27,1
+fill 1,1f1f1f,28,1
+fill 1,0f0f0f,29,1
+fill 1,070707,30,1
+fill 1,030303,31,1
+fill 1,010101,32,1
+fill 1,000000,33,1
+fill 1,010101,34,1
+fill 1,030303,35,1
+fill 1,070707,36,1
+fill 1,0f0f0f,37,1
+fill 1,1f1f1f,38,1
+fill 1,3f3f3f,39,1
+fill 1,7f7f7f,40,1
+fill 1,ffffff,41,11
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,25
+fill 1,7f7f7f,25,1
+fill 1,3f3f3f,26,1
+fill 1,1f1f1f,27,1
+fill 1,0f0f0f,28,1
+fill 1,070707,29,1
+fill 1,030303,30,1
+fill 1,010101,31,1
+fill 1,000000,32,3
+fill 1,010101,35,1
+fill 1,030303,36,1
+fill 1,070707,37,1
+fill 1,0f0f0f,38,1
+fill 1,1f1f1f,39,1
+fill 1,3f3f3f,40,1
+fill 1,7f7f7f,41,1
+fill 1,ffffff,42,10
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,24
+fill 1,7f7f7f,24,1
+fill 1,3f3f3f,25,1
+fill 1,1f1f1f,26,1
+fill 1,0f0f0f,27,1
+fill 1,070707,28,1
+fill 1,030303,29,1
+fill 1,010101,30,1
+fill 1,000000,31,5
+fill 1,010101,36,1
+fill 1,030303,37,1
+fill 1,070707,38,1
+fill 1,0f0f0f,39,1
+fill 1,1f1f1f,40,1
+fill 1,3f3f3f,41,1
+fill 1,7f7f7f,42,1
+fill 1,ffffff,43,9
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,23
+fill 1,7f7f7f,23,1
+fill 1,3f3f3f,24,1
+fill 1,1f1f1f,25,1
+fill 1,0f0f0f,26,1
+fill 1,070707,27,1
+fill 1,030303,28,1
+fill 1,010101,29,1
+fill 1,000000,30,7
+fill 1,010101,37,1
+fill 1,030303,38,1
+fill 1,070707,39,1
+fill 1,0f0f0f,40,1
+fill 1,1f1f1f,41,1
+fill 1,3f3f3f,42,1
+fill 1,7f7f7f,43,1
+fill 1,ffffff,44,8
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,22
+fill 1,7f7f7f,22,1
+fill 1,3f3f3f,23,1
+fill 1,1f1f1f,24,1
+fill 1,0f0f0f,25,1
+fill 1,070707,26,1
+fill 1,030303,27,1
+fill 1,010101,28,1
+fill 1,000000,29,9
+fill 1,010101,38,1
+fill 1,030303,39,1
+fill 1,070707,40,1
+fill 1,0f0f0f,41,1
+fill 1,1f1f1f,42,1
+fill 1,3f3f3f,43,1
+fill 1,7f7f7f,44,1
+fill 1,ffffff,45,7
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,21
+fill 1,7f7f7f,21,1
+fill 1,3f3f3f,22,1
+fill 1,1f1f1f,23,1
+fill 1,0f0f0f,24,1
+fill 1,070707,25,1
+fill 1,030303,26,1
+fill 1,010101,27,1
+fill 1,000000,28,11
+fill 1,010101,39,1
+fill 1,030303,40,1
+fill 1,070707,41,1
+fill 1,0f0f0f,42,1
+fill 1,1f1f1f,43,1
+fill 1,3f3f3f,44,1
+fill 1,7f7f7f,45,1
+fill 1,ffffff,46,6
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,20
+fill 1,7f7f7f,20,1
+fill 1,3f3f3f,21,1
+fill 1,1f1f1f,22,1
+fill 1,0f0f0f,23,1
+fill 1,070707,24,1
+fill 1,030303,25,1
+fill 1,010101,26,1
+fill 1,000000,27,13
+fill 1,010101,40,1
+fill 1,030303,41,1
+fill 1,070707,42,1
+fill 1,0f0f0f,43,1
+fill 1,1f1f1f,44,1
+fill 1,3f3f3f,45,1
+fill 1,7f7f7f,46,1
+fill 1,ffffff,47,5
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,19
+fill 1,7f7f7f,19,1
+fill 1,3f3f3f,20,1
+fill 1,1f1f1f,21,1
+fill 1,0f0f0f,22,1
+fill 1,070707,23,1
+fill 1,030303,24,1
+fill 1,010101,25,1
+fill 1,000000,26,15
+fill 1,010101,41,1
+fill 1,030303,42,1
+fill 1,070707,43,1
+fill 1,0f0f0f,44,1
+fill 1,1f1f1f,45,1
+fill 1,3f3f3f,46,1
+fill 1,7f7f7f,47,1
+fill 1,ffffff,48,4
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,18
+fill 1,7f7f7f,18,1
+fill 1,3f3f3f,19,1
+fill 1,1f1f1f,20,1
+fill 1,0f0f0f,21,1
+fill 1,070707,22,1
+fill 1,030303,23,1
+fill 1,010101,24,1
+fill 1,000000,25,17
+fill 1,010101,42,1
+fill 1,030303,43,1
+fill 1,070707,44,1
+fill 1,0f0f0f,45,1
+fill 1,1f1f1f,46,1
+fill 1,3f3f3f,47,1
+fill 1,7f7f7f,48,1
+fill 1,ffffff,49,3
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,17
+fill 1,7f7f7f,17,1
+fill 1,3f3f3f,18,1
+fill 1,1f1f1f,19,1
+fill 1,0f0f0f,20,1
+fill 1,070707,21,1
+fill 1,030303,22,1
+fill 1,010101,23,1
+fill 1,000000,24,19
+fill 1,010101,43,1
+fill 1,030303,44,1
+fill 1,070707,45,1
+fill 1,0f0f0f,46,1
+fill 1,1f1f1f,47,1
+fill 1,3f3f3f,48,1
+fill 1,7f7f7f,49,1
+fill 1,ffffff,50,2
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,16
+fill 1,7f7f7f,16,1
+fill 1,3f3f3f,17,1
+fill 1,1f1f1f,18,1
+fill 1,0f0f0f,19,1
+fill 1,070707,20,1
+fill 1,030303,21,1
+fill 1,010101,22,1
+fill 1,000000,23,21
+fill 1,010101,44,1
+fill 1,030303,45,1
+fill 1,070707,46,1
+fill 1,0f0f0f,47,1
+fill 1,1f1f1f,48,1
+fill 1,3f3f3f,49,1
+fill 1,7f7f7f,50,1
+fill 1,ffffff,51,1
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,15
+fill 1,7f7f7f,15,1
+fill 1,3f3f3f,16,1
+fill 1,1f1f1f,17,1
+fill 1,0f0f0f,18,1
+fill 1,070707,19,1
+fill 1,030303,20,1
+fill 1,010101,21,1
+fill 1,000000,22,23
+fill 1,010101,45,1
+fill 1,030303,46,1
+fill 1,070707,47,1
+fill 1,0f0f0f,48,1
+fill 1,1f1f1f,49,1
+fill 1,3f3f3f,50,1
+fill 1,7f7f7f,51,1
+render
+delay 200
+
+fill 1
+fill 1,ffffff,0,14
+fill 1,7f7f7f,14,1
+fill 1,3f3f3f,15,1
+fill 1,1f1f1f,16,1
+fill 1,0f0f0f,17,1
+fill 1,070707,18,1
+fill 1,030303,19,1
+fill 1,010101,20,1
+fill 1,000000,21,25
+fill 1,010101,46,1
+fill 1,030303,47,1
+fill 1,070707,48,1
+fill 1,0f0f0f,49,1
+fill 1,1f1f1f,50,1
+fill 1,3f3f3f,51,1
+render
+delay 200
+
+fill 1
+fill 1,7f7f7f,0,1
+fill 1,ffffff,1,12
+fill 1,7f7f7f,13,1
+fill 1,3f3f3f,14,1
+fill 1,1f1f1f,15,1
+fill 1,0f0f0f,16,1
+fill 1,070707,17,1
+fill 1,030303,18,1
+fill 1,010101,19,1
+fill 1,000000,20,27
+fill 1,010101,47,1
+fill 1,030303,48,1
+fill 1,070707,49,1
+fill 1,0f0f0f,50,1
+fill 1,1f1f1f,51,1
+render
+delay 200
+
+fill 1
+fill 1,3f3f3f,0,1
+fill 1,7f7f7f,1,1
+fill 1,ffffff,2,10
+fill 1,7f7f7f,12,1
+fill 1,3f3f3f,13,1
+fill 1,1f1f1f,14,1
+fill 1,0f0f0f,15,1
+fill 1,070707,16,1
+fill 1,030303,17,1
+fill 1,010101,18,1
+fill 1,000000,19,29
+fill 1,010101,48,1
+fill 1,030303,49,1
+fill 1,070707,50,1
+fill 1,0f0f0f,51,1
+render
+delay 200
+
+fill 1
+fill 1,1f1f1f,0,1
+fill 1,3f3f3f,1,1
+fill 1,7f7f7f,2,1
+fill 1,ffffff,3,8
+fill 1,7f7f7f,11,1
+fill 1,3f3f3f,12,1
+fill 1,1f1f1f,13,1
+fill 1,0f0f0f,14,1
+fill 1,070707,15,1
+fill 1,030303,16,1
+fill 1,010101,17,1
+fill 1,000000,18,31
+fill 1,010101,49,1
+fill 1,030303,50,1
+fill 1,070707,51,1
+render
+delay 200
+
+fill 1
+fill 1,0f0f0f,0,1
+fill 1,1f1f1f,1,1
+fill 1,3f3f3f,2,1
+fill 1,7f7f7f,3,1
+fill 1,ffffff,4,6
+fill 1,7f7f7f,10,1
+fill 1,3f3f3f,11,1
+fill 1,1f1f1f,12,1
+fill 1,0f0f0f,13,1
+fill 1,070707,14,1
+fill 1,030303,15,1
+fill 1,010101,16,1
+fill 1,000000,17,33
+fill 1,010101,50,1
+fill 1,030303,51,1
+render
+delay 200
+
+fill 1
+fill 1,070707,0,1
+fill 1,0f0f0f,1,1
+fill 1,1f1f1f,2,1
+fill 1,3f3f3f,3,1
+fill 1,7f7f7f,4,1
+fill 1,ffffff,5,4
+fill 1,7f7f7f,9,1
+fill 1,3f3f3f,10,1
+fill 1,1f1f1f,11,1
+fill 1,0f0f0f,12,1
+fill 1,070707,13,1
+fill 1,030303,14,1
+fill 1,010101,15,1
+fill 1,000000,16,35
+fill 1,010101,51,1
+render
+delay 200
+
+fill 1
+fill 1,030303,0,1
+fill 1,070707,1,1
+fill 1,0f0f0f,2,1
+fill 1,1f1f1f,3,1
+fill 1,3f3f3f,4,1
+fill 1,7f7f7f,5,1
+fill 1,ffffff,6,2
+fill 1,7f7f7f,8,1
+fill 1,3f3f3f,9,1
+fill 1,1f1f1f,10,1
+fill 1,0f0f0f,11,1
+fill 1,070707,12,1
+fill 1,030303,13,1
+fill 1,010101,14,1
+fill 1,000000,15,37
+render
+delay 200
+
+fill 1
+fill 1,010101,0,1
+fill 1,030303,1,1
+fill 1,070707,2,1
+fill 1,0f0f0f,3,1
+fill 1,1f1f1f,4,1
+fill 1,3f3f3f,5,1
+fill 1,7f7f7f,6,2
+fill 1,3f3f3f,8,1
+fill 1,1f1f1f,9,1
+fill 1,0f0f0f,10,1
+fill 1,070707,11,1
+fill 1,030303,12,1
+fill 1,010101,13,1
+fill 1,000000,14,38
+render
+delay 200
+
+fill 1
+fill 1,000000,0,1
+fill 1,010101,1,1
+fill 1,030303,2,1
+fill 1,070707,3,1
+fill 1,0f0f0f,4,1
+fill 1,1f1f1f,5,1
+fill 1,3f3f3f,6,2
+fill 1,1f1f1f,8,1
+fill 1,0f0f0f,9,1
+fill 1,070707,10,1
+fill 1,030303,11,1
+fill 1,010101,12,1
+fill 1,000000,13,39
+render
+delay 200
+
+fill 1
+fill 1,000000,0,2
+fill 1,010101,2,1
+fill 1,030303,3,1
+fill 1,070707,4,1
+fill 1,0f0f0f,5,1
+fill 1,1f1f1f,6,2
+fill 1,0f0f0f,8,1
+fill 1,070707,9,1
+fill 1,030303,10,1
+fill 1,010101,11,1
+fill 1,000000,12,40
+render
+delay 200
+
+fill 1
+fill 1,000000,0,3
+fill 1,010101,3,1
+fill 1,030303,4,1
+fill 1,070707,5,1
+fill 1,0f0f0f,6,2
+fill 1,070707,8,1
+fill 1,030303,9,1
+fill 1,010101,10,1
+fill 1,000000,11,41
+render
+delay 200
+
+fill 1
+fill 1,000000,0,4
+fill 1,010101,4,1
+fill 1,030303,5,1
+fill 1,070707,6,2
+fill 1,030303,8,1
+fill 1,010101,9,1
+fill 1,000000,10,42
+render
+delay 200
+
+fill 1
+fill 1,000000,0,5
+fill 1,010101,5,1
+fill 1,030303,6,2
+fill 1,010101,8,1
+fill 1,000000,9,43
+render
+delay 200
+
+fill 1
+fill 1,000000,0,6
+fill 1,010101,6,2
+fill 1,000000,8,44
+render
+delay 200
+
+fill 1
+fill 1,000000,0,52
+render
+delay 200
